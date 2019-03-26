42811940819dd922f1702db43a5cfb0aba8b9f0d
Vedmaka <god.vedmaka@gmail.com>
2019-03-26 12:25:41 +0300

    - Adds support for regular form fields like text inputs, textarea, etc - Adds eslint support - A bit of reformatting

--- resources/mw_acrolinx.js
+++ resources/mw_acrolinx.js
@@ -8,18 +8,18 @@
 
 'use strict';
 
-//$('#bodyContent').prepend( '<div id="acrolinxSidebar"><div id="acrolinxToggle"><div id="acrolinxContainer"></div></div>' );
-$('#bodyContent').prepend( '<div id="acrolinxContainer"></div>' );
+// $('#bodyContent').prepend( '<div id="acrolinxSidebar"><div id="acrolinxToggle"><div id="acrolinxContainer"></div></div>' );
+$( '#bodyContent' ).prepend( '<div id="acrolinxContainer"></div>' );
 
 var basicConf = {
 	sidebarContainerId: 'acrolinxContainer',
-	//See: https://cdn.rawgit.com/acrolinx/acrolinx-sidebar-demo/v0.3.52/doc/pluginDoc/interfaces/_src_acrolinx_libs_plugin_interfaces_.initparameters.html
+	// See: https://cdn.rawgit.com/acrolinx/acrolinx-sidebar-demo/v0.3.52/doc/pluginDoc/interfaces/_src_acrolinx_libs_plugin_interfaces_.initparameters.html
 	serverAddress: mw.config.get( 'wgAcrolinxServerAddress' ),
 	clientSignature: mw.config.get( 'wgAcrolinxClientSignature' ),
-	clientLocale: mw.config.get('wgAcrolinxUserLanguage' ),
+	clientLocale: mw.config.get( 'wgAcrolinxUserLanguage' ),
 
 	checkSettings: {
-		'language': mw.config.get('wgAcrolinxPageLanguage' )
+		language: mw.config.get( 'wgAcrolinxPageLanguage' )
 	},
 
 	/**
@@ -36,47 +36,59 @@ var basicConf = {
 
 };
 
-var acrolinxPlugin = new acrolinx.plugins.AcrolinxPlugin(basicConf);
-var multiAdapter = new acrolinx.plugins.adapter.MultiEditorAdapter({});
+var acrolinxPlugin = new acrolinx.plugins.AcrolinxPlugin( basicConf );
+var multiAdapter = new acrolinx.plugins.adapter.MultiEditorAdapter( {} );
 
-jQuery.fn.addAcrolinxAdapterToFormInput = function() {
-	return this.each( function() {
-		var textareaID = $(this).attr('id');
-		if ( $(this).hasClass('tinymce') ) {
-			var inputAdapter = new acrolinx.plugins.adapter.TinyMCEAdapter({editorId: textareaID});
+jQuery.fn.addAcrolinxAdapterToFormInput = function () {
+	return this.each( function () {
+		var inputAdapter, textareaID = $( this ).attr( 'id' );
+
+		if ( $( this ).hasClass( 'tinymce' ) ) {
+			inputAdapter = new acrolinx.plugins.adapter.TinyMCEAdapter( {
+				editorId: textareaID
+			} );
 		} else {
-			var inputAdapter = new acrolinx.plugins.adapter.InputAdapter({editorId: textareaID});
+			inputAdapter = new acrolinx.plugins.adapter.InputAdapter( {
+				editorId: textareaID
+			} );
 		}
-		multiAdapter.addSingleAdapter(inputAdapter);
-	});
+		multiAdapter.addSingleAdapter( inputAdapter );
+	} );
 };
 
-var action = mw.config.get('wgAction');
-if ( action == 'edit' ) {
-	var inputAdapter = new acrolinx.plugins.adapter.InputAdapter({editorId: 'wpTextbox1'});
-	multiAdapter.addSingleAdapter(inputAdapter);
-} else if ( action == 'tinymceedit' ) {
-	var tinyMceAdapter = new acrolinx.plugins.adapter.TinyMCEAdapter({editorId: 'wpTextbox1'});
-	multiAdapter.addSingleAdapter(tinyMceAdapter);
+var action = mw.config.get( 'wgAction' );
+if ( action === 'edit' ) {
+	var inputAdapter = new acrolinx.plugins.adapter.InputAdapter( {
+		editorId: 'wpTextbox1'
+	} );
+	multiAdapter.addSingleAdapter( inputAdapter );
+} else if ( action === 'tinymceedit' ) {
+	var tinyMceAdapter = new acrolinx.plugins.adapter.TinyMCEAdapter( {
+		editorId: 'wpTextbox1'
+	} );
+	multiAdapter.addSingleAdapter( tinyMceAdapter );
 } else {
 	// This is most likely a Page Forms editing interface, which will have
 	// an action of either 'formedit' or 'view', depending on the URL.
-	$('textarea').not('.multipleTemplateStarter textarea').addAcrolinxAdapterToFormInput();
+	$( 'textarea, input.createboxInput' )
+		.not( '.multipleTemplateStarter textarea' )
+		.not( '.multipleTemplateStarter input.createboxInput' )
+		.addAcrolinxAdapterToFormInput();
 }
 
-acrolinxPlugin.registerAdapter(multiAdapter);
+acrolinxPlugin.registerAdapter( multiAdapter );
 acrolinxPlugin.init();
 
 // Use a JavaScript hook to also add an adapter for any textarea in a
 // newly-created instane of a multiple-instance template.
-mw.hook('pf.addTemplateInstance').add( function( $newDiv ) {
-	$newDiv.find('textarea').addAcrolinxAdapterToFormInput();
-});
+mw.hook( 'pf.addTemplateInstance' ).add( function ( $newDiv ) {
+	$newDiv.find( 'textarea' ).addAcrolinxAdapterToFormInput();
+} );
 
 function getContentAreaWidthWithAcrolinx( acrolinxWidth ) {
-	var browserWidth = $(window).width();
-	var mainTextLeft = $('#mw-content-text').offset().left;
-	acrolinxWidth = acrolinxWidth || $('#acrolinxContainer iframe').width();
+	var browserWidth = $( window ).width();
+	var mainTextLeft = $( '#mw-content-text' ).offset().left;
+	acrolinxWidth = acrolinxWidth || $( '#acrolinxContainer iframe' ).width();
 	return browserWidth - mainTextLeft - acrolinxWidth - 50;
 }
 
@@ -86,31 +98,34 @@ function getContentAreaWidthWithAcrolinx( acrolinxWidth ) {
  */
 function resizeForAcrolinx() {
 	var newContentAreaWidth = getContentAreaWidthWithAcrolinx();
-	$('#mw-content-text').width( newContentAreaWidth );
+	$( '#mw-content-text' ).width( newContentAreaWidth );
 }
 
 resizeForAcrolinx();
-$(window).resize( function() {
+$( window ).resize( function () {
 	resizeForAcrolinx();
-});
+} );
 
-$('#acrolinxContainer').prepend('<div id="acrolinxToggle">&gt;</div>');
+$( '#acrolinxContainer' ).prepend( '<div id="acrolinxToggle">&gt;</div>' );
 
-$('div#acrolinxToggle').click(function() {
-	if ( $(this).attr('minimized') == 'true' ) {
-		$(this).html( '>' );
-		$(this).removeAttr('minimized');
-		var acrolinxWidth = 300;
+$( 'div#acrolinxToggle' ).click( function () {
+
+	var acrolinxWidth;
+
+	if ( $( this ).attr( 'minimized' ) === 'true' ) {
+		$( this ).html( '>' );
+		$( this ).removeAttr( 'minimized' );
+		acrolinxWidth = 300;
 	} else {
-		$(this).html( '<' );
-		$(this).attr('minimized', 'true');
-		var acrolinxWidth = 10;
+		$( this ).html( '<' );
+		$( this ).attr( 'minimized', 'true' );
+		acrolinxWidth = 10;
 	}
-	$('#acrolinxContainer iframe').animate({
-		width: acrolinxWidth + "px"
-	});
+	$( '#acrolinxContainer iframe' ).animate( {
+		width: acrolinxWidth + 'px'
+	} );
 	var contentAreaWidth = getContentAreaWidthWithAcrolinx( acrolinxWidth );
-	$('#mw-content-text').animate({
+	$( '#mw-content-text' ).animate( {
 		width: contentAreaWidth
-	});
-});
+	} );
+} );
